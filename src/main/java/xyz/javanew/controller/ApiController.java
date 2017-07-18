bf8547c83d0c696623b88fb09e495df3deb39210
yanwg <yanwg@upc1129.uz.local>
2017-07-18 11:33:02 +0800

    Api完善

--- src/main/java/xyz/javanew/controller/ApiController.java
+++ src/main/java/xyz/javanew/controller/ApiController.java
@@ -37,56 +37,57 @@ import xyz.javanew.util.QrcodeUtil.QrcodeInfo;
 @Controller
 @RequestMapping("api")
 public class ApiController {
-	private static final Logger logger = Logger.getLogger(ApiController.class);
-	@Autowired
-	private CacheService cacheService;
+    private static final Logger logger = Logger.getLogger(ApiController.class);
+    @Autowired
+    private CacheService cacheService;
 
-	@RequestMapping("qrcode")
-	public void qrcode(HttpServletRequest request, HttpServletResponse response, ModelMap map, QrcodeInfo qrcodeInfo) {
-		Result<String> result = new Result<String>();
-		try {
-			String uploadAbsolutePath = cacheService.getUploadAbsolutePath();
-			qrcodeInfo.setQrcodePath(uploadAbsolutePath);
-			String baseUrl = cacheService.getBaseUrl(SysConfig.BASE_URL);
-			String logoPath = qrcodeInfo.getLogoPath();
-			if (StringUtils.hasText(logoPath)) {
-				logoPath = qrcodeInfo.getLogoPath().replace(baseUrl + "/" + cacheService.getUploadFolder(), uploadAbsolutePath);
-				qrcodeInfo.setLogoPath(logoPath);
-			}
-			qrcodeInfo.setQrcodeName("temp-qrcode");
-			qrcodeInfo.setBackground(qrcodeInfo.getBackground().replace("#", ""));
-			qrcodeInfo.setForeground(qrcodeInfo.getForeground().replace("#", ""));
-			String generateQrcode = QrcodeUtil.generateQrcode(qrcodeInfo);
-			// 读取文件
-			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(generateQrcode));
-			OutputStream out = response.getOutputStream();
-			// 写文件
-			byte[] buffer = new byte[1024];
-			while (bis.read(buffer) != -1) {
-				out.write(buffer);
-			}
-			bis.close();
-			out.close();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-	}
+    @RequestMapping(value = { "qrcode" }, produces = { "application/json;charset=UTF-8" })
+    public void qrcode(HttpServletRequest request, HttpServletResponse response, ModelMap map, QrcodeInfo qrcodeInfo) {
+        try {
+            String uploadAbsolutePath = cacheService.getUploadAbsolutePath();
+            qrcodeInfo.setQrcodePath(uploadAbsolutePath);
+            String baseUrl = cacheService.getBaseUrl(SysConfig.BASE_URL);
+            String logoPath = qrcodeInfo.getLogoPath();
+            if (StringUtils.hasText(logoPath)) {
+                logoPath =
+                        qrcodeInfo.getLogoPath().replace(baseUrl + "/" + cacheService.getUploadFolder(),
+                                uploadAbsolutePath);
+                qrcodeInfo.setLogoPath(logoPath);
+            }
+            qrcodeInfo.setQrcodeName("temp-qrcode");
+            qrcodeInfo.setBackground(qrcodeInfo.getBackground().replace("#", ""));
+            qrcodeInfo.setForeground(qrcodeInfo.getForeground().replace("#", ""));
+            String generateQrcode = QrcodeUtil.generateQrcode(qrcodeInfo);
+            // 读取文件
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(generateQrcode));
+            OutputStream out = response.getOutputStream();
+            // 写文件
+            byte[] buffer = new byte[1024];
+            while (bis.read(buffer) != -1) {
+                out.write(buffer);
+            }
+            bis.close();
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
 
-	@RequestMapping(value = { "/qrcode/parse" })
-	public @ResponseBody Result<String> parseQrcode(HttpServletRequest request, ModelMap map, String qrcodeImage) {
-		Result<String> result = new Result<String>();
-		try {
-			String uploadAbsolutePath = cacheService.getUploadAbsolutePath();
-			String baseUrl = cacheService.getBaseUrl(SysConfig.BASE_URL);
-			if (StringUtils.hasText(qrcodeImage)) {
-				qrcodeImage = qrcodeImage.replace(baseUrl + "/" + cacheService.getUploadFolder(), uploadAbsolutePath);
-			}
-			String qrcodeText = QrcodeUtil.parseQrcode(qrcodeImage);
-			result.setData(qrcodeText);
-			result.setResultStatusAndMsg(ResultType.SUCCESS, null);
-		} catch (Exception e) {
-			result.setResultStatusAndMsg(ResultType.SERVICE_ERROR, null);
-		}
-		return result;
-	}
+    @RequestMapping(value = { "/qrcode/parse" }, produces = { "application/json;charset=UTF-8" })
+    public @ResponseBody Result<String> parseQrcode(HttpServletRequest request, ModelMap map, String qrcodeImage) {
+        Result<String> result = new Result<String>();
+        try {
+            String uploadAbsolutePath = cacheService.getUploadAbsolutePath();
+            String baseUrl = cacheService.getBaseUrl(SysConfig.BASE_URL);
+            if (StringUtils.hasText(qrcodeImage)) {
+                qrcodeImage = qrcodeImage.replace(baseUrl + "/" + cacheService.getUploadFolder(), uploadAbsolutePath);
+            }
+            String qrcodeText = QrcodeUtil.parseQrcode(qrcodeImage);
+            result.setData(qrcodeText);
+            result.setResultStatusAndMsg(ResultType.SUCCESS, null);
+        } catch (Exception e) {
+            result.setResultStatusAndMsg(ResultType.SERVICE_ERROR, null);
+        }
+        return result;
+    }
 }
